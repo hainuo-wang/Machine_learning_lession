6cd4af492ef2e0c32082b62da8697ac366ddd08a
whn521 <88794833+whn521@users.noreply.github.com>
2022-11-05 21:30:37 +0800

    20221105

--- dataset_and_dataloader/myDatadet_use.py
+++ dataset_and_dataloader/myDatadet_use.py
@@ -1,10 +1,25 @@
+import torchvision.transforms
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
 from read_data import read_split_data
 from MyDataset import MyDataSet
 
 train_images_path, train_images_label, val_images_path, val_images_label = read_split_data('hymenoptera_data')
 print(train_images_label[0])
-train_dataset = MyDataSet(train_images_path, train_images_label)
-val_dataset = MyDataSet(val_images_path, val_images_label)
+transform = torchvision.transforms.ToTensor()
+train_dataset = MyDataSet(train_images_path, train_images_label, transform=transform)
+val_dataset = MyDataSet(val_images_path, val_images_label, transform=transform)
 train_loader = DataLoader(train_dataset, batch_size=64)
 val_loader = DataLoader(val_dataset, batch_size=64)
+
+writer = SummaryWriter('log1')
+for i in range(10):
+    img, label = train_dataset[i]
+    writer.add_image('set2', img, i)
+writer.close()
+
+# tensorboard使用方法：复制writer = SummaryWriter('log1')中log1的绝对地址，然后
+# 在终端（terminal）输入：tensorboard --logdir=绝对地址
+# 比如我这里就是：tensorboard --logdir=D:\project\pycharm\Machine_learning_lession\dataset_and_dataloader\log1
+# 然后回车，点击有6006的网址即可进入tensorboard界面
