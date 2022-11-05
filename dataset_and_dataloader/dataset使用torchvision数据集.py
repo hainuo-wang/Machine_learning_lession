5c0d393c64217e311dd31312c660221a3751c05f
whn521 <88794833+whn521@users.noreply.github.com>
2022-11-05 21:20:16 +0800

    20221105

--- dataset_and_dataloader/dataset使用torchvision数据集.py
+++ dataset_and_dataloader/dataset使用torchvision数据集.py
@@ -19,3 +19,8 @@ writer.close()
 train_loader = DataLoader(train_dataset, batch_size=64)
 test_loader = DataLoader(test_dataset, batch_size=64)
 
+# tensorboard使用方法：复制writer = SummaryWriter('log1')中log1的绝对地址，然后
+# 在终端（terminal）输入：tensorboard --logdir=绝对地址
+# 比如我这里就是：tensorboard --logdir=D:\project\pycharm\Machine_learning_lession\dataset_and_dataloader\log1
+# 然后回车，点击有6006的网址即可进入tensorboard界面
+
