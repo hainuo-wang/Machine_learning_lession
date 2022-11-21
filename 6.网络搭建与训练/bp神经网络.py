9367a9678b44c2b3b73d9aa821c64ee7fcb5963a
whn521 <88794833+whn521@users.noreply.github.com>
2022-11-21 23:25:51 +0800

    20221120

--- 6.网络搭建与训练/bp神经网络.py
+++ 6.网络搭建与训练/bp神经网络.py
@@ -1,21 +1,41 @@
 import numpy as np
 import torch
+from sklearn import preprocessing
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
 
-xy = np.loadtxt('../data/bp神经网络.txt', delimiter='	', dtype=np.float32)
+xy = np.loadtxt('Real estate valuation data set.txt', delimiter='	', dtype=np.float32)
 x_data = torch.from_numpy(xy[:, :-1])
 y_data = torch.from_numpy(xy[:, [-1]])
 mean, std = torch.mean(x_data), torch.std(x_data)
-x_data = (x_data-mean)/std
+x_data = (x_data - mean) / std
 mean, std = torch.mean(y_data), torch.std(y_data)
-y_data = (y_data-mean)/std
+y_data = (y_data - mean) / std
 x_data = torch.nn.functional.normalize(x_data, dim=0)
 y_data = torch.nn.functional.normalize(y_data, dim=0)
+
+# print(x_data.shape, y_data.shape)
+#
+x_train, x_test = train_test_split(x_data, test_size=0.2)
+y_train, y_test = train_test_split(y_data, test_size=0.2)
+
+
+# min_max_scaler = preprocessing.MinMaxScaler()
+# x_data = min_max_scaler.fit_transform(x_data)
+# y_data = min_max_scaler.fit_transform(y_data)
+# x_data = torch.FloatTensor(x_data)
+# y_data = torch.FloatTensor(y_data)
+# x_data = torch.nn.functional.normalize(x_data, dim=0)
+# y_data = torch.nn.functional.normalize(y_data, dim=0)
+
+# print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+# exit()
 
 
 class Model(torch.nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.linear1 = torch.nn.Linear(7, 4)
+        self.linear1 = torch.nn.Linear(6, 4)
         self.linear2 = torch.nn.Linear(4, 2)
         self.linear3 = torch.nn.Linear(2, 1)
         self.sigmoid = torch.nn.Sigmoid()
@@ -33,13 +53,19 @@ optimizer = torch.optim.SGD(model.parameters(), lr=0.1)  # 优化函数，随机
 
 for epoch in range(100):
     # 前馈
-    y_pred = model(x_data)
-    loss = criterion(y_pred, y_data)
-    print(epoch, loss.item())
-
+    y_pred = model(x_train)
+    loss = criterion(y_pred, y_train)
+    print("--------epoch{}---------".format(epoch))
+    print("loss", loss.item())
     # 反馈
     optimizer.zero_grad()
     loss.backward()
-
     # 更新
     optimizer.step()
+    correct = 0
+    total = 0
+    with torch.no_grad():  # 下面是没有梯度的计算,主要是测试集使用，不需要再计算梯度了
+        outputs = model(x_test)
+        print("均方误差", mean_squared_error(y_test, outputs))
+        print("平方绝对误差", mean_absolute_error(y_test, outputs))
+        print("------------------------")
